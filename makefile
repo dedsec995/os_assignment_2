d0b3e49c9ce2573360273b4fa0067b36b6553172
dedsec995 <amitluhar49@gmail.com>
2023-07-12 22:30:18 -0400

    Before Submitting

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 tlb: tlb.c
 	gcc tlb.c -o tlb -lm 
 run:
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_small_addr_range 2 20 > test.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_small_addr_range 0 20 > test.txt
 
 clean:
 	rm -rf tlb && rm -rf test.txt && clear 
@@ -11,17 +11,3 @@ demo: demo.c
 
 ddemo:
 	rm -rf demo && clear
-
-makeall:
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_small_addr_range 0 20 > zzfifo0.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_large_addr_range 0 20 > zzfifo1.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_small_addr_range 0 20 > zzfifo2.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_large_addr_range 0 20 > zzfifo3.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_small_addr_range 1 20 > zzrand0.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_large_addr_range 1 20 > zzrand1.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_small_addr_range 1 20 > zzrand2.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_large_addr_range 1 20 > zzrand3.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_small_addr_range 2 20 > zzclock0.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_large_addr_range 2 20 > zzclock1.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_small_addr_range 2 20 > zzclock2.txt
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_large_addr_range 2 20 > zzclock3.txt
--- tlb.c
+++ tlb.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 #include <math.h>
 
-char ADDRESS_SPACE_SIZE = 16; // 16-bit address space
-
 typedef struct {
-    unsigned int vpn; // Virtual Page Number
-    unsigned int ppn; // Physical Page Number
+    unsigned int vpn;
+    unsigned int ppn;
+    unsigned int valid;
 } PageTableEntry;
 
 typedef struct {
-    unsigned int vpn; // Virtual Page Number
-    unsigned int ppn; // Physical Page Number
+    unsigned int vpn; 
+    unsigned int ppn;
+    unsigned int valid;
+} MemoryEntry;
+
+typedef struct {
+    unsigned int vpn; 
+    unsigned int ppn; 
     int referenced;
 } TLBEntry;
 
+char ADDRESS_SPACE_SIZE = 16; // 16-bit address space
 char* PAGE_SIZE_B = NULL; // ? byte page size
 short unsigned int* NEW_ADDRESS = NULL; // New Virtual Address to service
 short unsigned int* TLB_SIZE = NULL; // ? Translation Lookaside Buffer entries
@@ -31,7 +37,9 @@ int main(int argc, char* argv[]) {
     int PAGE_FAULTS = 0;
     int TLB_HITS = 0;
     int TLB_MISSES = 0;
-    int clock_hand = 0;
+    int clock_point = 0;
+
+    //---------------------Binary File Loading and Reading-------------------------------
 
     FILE* fp;
     PAGE_SIZE_B = malloc(1);
@@ -46,15 +54,21 @@ int main(int argc, char* argv[]) {
     }
     fread(PAGE_SIZE_B, 1, 1, fp); // The first byte of the binary will be the page size
     fread(TLB_SIZE, 1, 1, fp);    // The second byte of the binary will be the TLB size
-    unsigned int PAGE_SIZE = *PAGE_SIZE_B;
-    int PAGE_TABLE_ENTRIES = pow(2, ADDRESS_SPACE_SIZE) / *PAGE_SIZE_B;
+
+    //---------------------Binary File Loading and Reading-------------------------------
+
+    int PAGE_TABLE_ENTRIES = 2048;
     PageTableEntry* PAGE_TABLE = NULL;
     PAGE_TABLE = malloc(PAGE_TABLE_ENTRIES * sizeof(PageTableEntry));
 
+    int PPN_MEMORY_ENTRIES = pow(2, ADDRESS_SPACE_SIZE) / *PAGE_SIZE_B;
+    MemoryEntry* MEMORY_TABLE = NULL;
+    MEMORY_TABLE = malloc(PPN_MEMORY_ENTRIES * sizeof(MemoryEntry));
+
     // Iterate 2 bytes at a time the correct number of PTEs to get the physical address mappings
-    for (int i = 0; i < PAGE_TABLE_ENTRIES; i++) {
-        if (fread(&PAGE_TABLE[i].ppn, 2, 1, fp) == 1) {
-            PAGE_TABLE[i].vpn = i; // Assign the Virtual Page Number to the corresponding Page Table Entry
+    for (int i = 0; i < PPN_MEMORY_ENTRIES; i++) {
+        if (fread(&MEMORY_TABLE[i].ppn, 2, 1, fp) == 1) {
+            MEMORY_TABLE[i].vpn = i; //
         }
         else {
             printf("Something went wrong with fread!\n");
@@ -62,9 +76,23 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    //-----------------------------------------Creating Page Table---------------------------------------------
+    
+    int page_table_index = 0;
+
+    for (int i = 0; i < PAGE_TABLE_ENTRIES; i++) {
+            PAGE_TABLE[i].vpn = MEMORY_TABLE[i].vpn;
+            PAGE_TABLE[i].ppn = PAGE_TABLE[i].ppn;
+            PAGE_TABLE[i].valid = PAGE_TABLE[i].valid;
+        }
+
+    //-----------------------------------------Creating Page Table---------------------------------------------
+
+
+    //-----------------------------------------Creating TLB---------------------------------------------
     int TLB_ENTRIES = *TLB_SIZE;
     TLBEntry* TLB = malloc(TLB_ENTRIES * sizeof(TLBEntry));
-    int tlbIndex = 0; // Index for the TLB replacement
+    int tlb_index = 0; // Index for the TLB replacement
 
     for (int i = 0; i < *TLB_SIZE; i++) {
         TLB[i].vpn = -1;
@@ -72,14 +100,18 @@ int main(int argc, char* argv[]) {
         TLB[i].referenced = 0;
     }
 
+    //-----------------------------------------Creating TLB---------------------------------------------
+
+
     while (fread(NEW_ADDRESS, 2, 1, fp) == 1) {
-        int tlbHit = 0;
+        int tlb_hit = 0;
+        int page_hit = 0;
         unsigned int PPN;
         unsigned int VPN;
         unsigned int offset;
         unsigned int TRANSLATED_PHYSICAL_ADDRESS;
 
-        VPN = *NEW_ADDRESS / *PAGE_SIZE_B; // Calculate the Virtual Page Number
+        VPN = *NEW_ADDRESS / *PAGE_SIZE_B;
         // printf("\nVPN:%hu\n",VPN);
         for (int i = 0; i < TLB_ENTRIES; i++) {
             // printf("TLB[%d]: %hu\n",i,TLB[i].vpn);
@@ -87,55 +119,72 @@ int main(int argc, char* argv[]) {
                 PPN = TLB[i].ppn;
                 offset = *NEW_ADDRESS % *PAGE_SIZE_B;
                 TRANSLATED_PHYSICAL_ADDRESS = (PPN * *PAGE_SIZE_B) + offset;
-                tlbHit = 1;
+                tlb_hit = 1;
                 TLB[i].referenced = 1;
                 TLB_HITS++;
                 // printf("Inside");
                 break;
             }
         }
-        if(!tlbHit){
-            if(REPL == 0){
+        if(!tlb_hit){
+            if(REPL == 0){ // FIFO
                 TLB_MISSES++;
-                PPN = PAGE_TABLE[VPN].ppn;
+                for(int i = 0; i < PAGE_TABLE_ENTRIES; i++){
+                    if(PAGE_TABLE[i].vpn == VPN){
+                        page_hit = 1;
+                        break;
+                    }
+                }
+                if(!page_hit){
+                    PPN = MEMORY_TABLE[VPN].ppn;
+                    offset = *NEW_ADDRESS % *PAGE_SIZE_B;
+                    TRANSLATED_PHYSICAL_ADDRESS = (PPN * *PAGE_SIZE_B) + offset;
+                    PAGE_TABLE[page_table_index].vpn = VPN;
+                    PAGE_TABLE[page_table_index].ppn = PPN;
+                    page_table_index++;
+                    if(page_table_index == PAGE_TABLE_ENTRIES){
+                        page_table_index = 0;
+                    }
+                    PAGE_FAULTS++;
+                    }
+                PPN = MEMORY_TABLE[VPN].ppn;
                 offset = *NEW_ADDRESS % *PAGE_SIZE_B;
                 TRANSLATED_PHYSICAL_ADDRESS = (PPN * *PAGE_SIZE_B) + offset;
-                TLB[tlbIndex].vpn = VPN;
-                TLB[tlbIndex].ppn = PPN;
-                tlbIndex++;
-                if(tlbIndex == TLB_ENTRIES){
-                    tlbIndex = 0;
+                TLB[tlb_index].vpn = VPN;
+                TLB[tlb_index].ppn = PPN;
+                tlb_index++;
+                if(tlb_index == TLB_ENTRIES){
+                    tlb_index = 0;
                 }
             }
-            else if(REPL == 1){
+            else if(REPL == 1){ // RAND
                 TLB_MISSES++;
-                PPN = PAGE_TABLE[VPN].ppn;
+                PPN = MEMORY_TABLE[VPN].ppn;
                 offset = *NEW_ADDRESS % *PAGE_SIZE_B;
                 TRANSLATED_PHYSICAL_ADDRESS = (PPN * *PAGE_SIZE_B) + offset;
                 int replaceIndex = rand() % *TLB_SIZE;
                 TLB[replaceIndex].vpn = VPN;
                 TLB[replaceIndex].ppn = PPN;
             }
-            else if(REPL == 2){
-                PPN = PAGE_TABLE[VPN].ppn;
+            else if(REPL == 2){ // CLOCK
+                PPN = MEMORY_TABLE[VPN].ppn;
                 offset = *NEW_ADDRESS % *PAGE_SIZE_B;
                 TRANSLATED_PHYSICAL_ADDRESS = (PPN * *PAGE_SIZE_B) + offset;
                 int replaced_entry_index = -1;
                 while (1) {
-                    clock_hand = (clock_hand + 1) % *TLB_SIZE;
-                    if (TLB[clock_hand].referenced == 0) {
-                        replaced_entry_index = clock_hand;
+                    clock_point = (clock_point + 1) % *TLB_SIZE;
+                    if (TLB[clock_point].referenced == 0) {
+                        replaced_entry_index = clock_point;
                         break;
                     } else {
-                        TLB[clock_hand].referenced = 0; // Reset the reference bit
+                        TLB[clock_point].referenced = 0;
                     }
                 }
 
-                // Update the TLB with the new entry
                 TLB[replaced_entry_index].vpn = VPN;
                 TLB[replaced_entry_index].ppn = PPN;
                 TLB[replaced_entry_index].referenced = 1;
-                clock_hand = replaced_entry_index;
+                clock_point = replaced_entry_index;
 
                 TLB_MISSES++;
             }
@@ -153,5 +202,6 @@ int main(int argc, char* argv[]) {
     free(NEW_ADDRESS);
     free(TLB_SIZE);
     free(PAGE_TABLE);
+    free(MEMORY_TABLE);
     fclose(fp);
 }
