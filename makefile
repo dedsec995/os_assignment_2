79615883ae0380ee4a6ec0dacba7eb3b1be8dc99
dedsec995 <amitluhar49@gmail.com>
2023-07-12 20:54:19 -0400

    Page table TLB and All 3 replacement policy works

--- makefile
+++ makefile
@@ -1,13 +1,27 @@
 tlb: tlb.c
 	gcc tlb.c -o tlb -lm 
 run:
-	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_small_addr_range 0
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_small_addr_range 2 20 > test.txt
 
 clean:
-	rm -rf tlb && clear
+	rm -rf tlb && rm -rf test.txt && clear 
 
-demo:
-	gcc demo.c -o demo -pthread && clear && ./demo
+demo: demo.c
+	gcc demo.c -o demo -lm && ./demo
 
 ddemo:
 	rm -rf demo && clear
+
+makeall:
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_small_addr_range 0 20 > zzfifo0.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_large_addr_range 0 20 > zzfifo1.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_small_addr_range 0 20 > zzfifo2.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_large_addr_range 0 20 > zzfifo3.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_small_addr_range 1 20 > zzrand0.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_large_addr_range 1 20 > zzrand1.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_small_addr_range 1 20 > zzrand2.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_large_addr_range 1 20 > zzrand3.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_small_addr_range 2 20 > zzclock0.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/30_large_addr_range 2 20 > zzclock1.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_small_addr_range 2 20 > zzclock2.txt
+	gcc tlb.c -o tlb -lm && ./tlb traces_and_answers/practice_files/10k_large_addr_range 2 20 > zzclock3.txt
--- tlb.c
+++ tlb.c
@@ -1,55 +1,157 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-char  ADDRESS_SPACE_SIZE = 16; //16 bit address space
-char  * PAGE_SIZE_B = NULL; //? byte page size
-short unsigned int * PPN = NULL; //Physical Page Number
-short unsigned int * NEW_ADDRESS = NULL; //New Virtual Address to service
-short   unsigned int * TLB_SIZE = NULL; //? Translation Lookaside Buffer entries
-unsigned char REPL; //0 for FIFO, 1 for Random, 2 for Clock 
+
+char ADDRESS_SPACE_SIZE = 16; // 16-bit address space
+
+typedef struct {
+    unsigned int vpn; // Virtual Page Number
+    unsigned int ppn; // Physical Page Number
+} PageTableEntry;
+
+typedef struct {
+    unsigned int vpn; // Virtual Page Number
+    unsigned int ppn; // Physical Page Number
+    int referenced;
+} TLBEntry;
+
+char* PAGE_SIZE_B = NULL; // ? byte page size
+short unsigned int* NEW_ADDRESS = NULL; // New Virtual Address to service
+short unsigned int* TLB_SIZE = NULL; // ? Translation Lookaside Buffer entries
+unsigned char REPL; // 0 for FIFO, 1 for Random, 2 for Clock
 int seed;
 
-int main(int argc, char * argv[]){
-    if(argc < 3 || argc > 4){//You will take in 3 arguments: a binary test file, the replacement algorithm (0, 1, or 2), and a seed for the Random replacement if used
+int main(int argc, char* argv[]) {
+    if (argc < 3 || argc > 4) { // You will take in 3 arguments: a binary test file, the replacement algorithm (0, 1, or 2), and a seed for the Random replacement if used
         printf("You must enter an input file to test and a replacement policy!\n");
         exit(-1);
     }
-    
-    FILE * fp;
+
+    // Initializing the counters
+    int PAGE_FAULTS = 0;
+    int TLB_HITS = 0;
+    int TLB_MISSES = 0;
+    int clock_hand = 0;
+
+    FILE* fp;
     PAGE_SIZE_B = malloc(1);
-    NEW_ADDRESS = malloc(2); 
-    PPN = malloc(2);
+    NEW_ADDRESS = malloc(2);
     TLB_SIZE = malloc(1);
-    
+
     fp = fopen(argv[1], "rb");
     REPL = atoi(argv[2]);
-    if(argc == 4){
+    if (argc == 4) {
         seed = atoi(argv[3]);
         srand(seed);
     }
-    fread(PAGE_SIZE_B, 1, 1, fp); //The first byte of the binary will be the page size
-    fread(TLB_SIZE, 1, 1, fp); //The second byte of the binary will be the TLB size
-    short int PAGE_TABLE_ENTRIES;
-    //PAGE_TABLE_ENTRIES = You must figure out how many Page Table Entries there are!
-    //Iterate 2 bytes at a time the correct number of PTEs to get the physical address mappings
-    for(int i = 0; i < PAGE_TABLE_ENTRIES; i++){
-        if (fread(PPN, 2, 1, fp) == 1){
-            //Here we read 1 (2 byte) Physical Address at a time; you must map it to the corresponding Page Table Entry
-        }else{
+    fread(PAGE_SIZE_B, 1, 1, fp); // The first byte of the binary will be the page size
+    fread(TLB_SIZE, 1, 1, fp);    // The second byte of the binary will be the TLB size
+    unsigned int PAGE_SIZE = *PAGE_SIZE_B;
+    int PAGE_TABLE_ENTRIES = pow(2, ADDRESS_SPACE_SIZE) / *PAGE_SIZE_B;
+    PageTableEntry* PAGE_TABLE = NULL;
+    PAGE_TABLE = malloc(PAGE_TABLE_ENTRIES * sizeof(PageTableEntry));
+
+    // Iterate 2 bytes at a time the correct number of PTEs to get the physical address mappings
+    for (int i = 0; i < PAGE_TABLE_ENTRIES; i++) {
+        if (fread(&PAGE_TABLE[i].ppn, 2, 1, fp) == 1) {
+            PAGE_TABLE[i].vpn = i; // Assign the Virtual Page Number to the corresponding Page Table Entry
+        }
+        else {
             printf("Something went wrong with fread!\n");
             exit(-1);
         }
     }
 
-    while (fread(NEW_ADDRESS, 2, 1, fp) == 1) {
-        //Below is how the print statement should be formatted
-        //printf("VA:%x -- PA:%x\n", VIRTUAL_ADDRESS, TRANSLATED_PHYSICAL_ADDRESS);
+    int TLB_ENTRIES = *TLB_SIZE;
+    TLBEntry* TLB = malloc(TLB_ENTRIES * sizeof(TLBEntry));
+    int tlbIndex = 0; // Index for the TLB replacement
+
+    for (int i = 0; i < *TLB_SIZE; i++) {
+        TLB[i].vpn = -1;
+        TLB[i].ppn = -1;
+        TLB[i].referenced = 0;
     }
-    //Below is how the print statement should be formatted
-    //printf("Page Faults: %d\nTLB hits: %d\nTlb misses: %d\n", PAGE_FAULTS, TLB_HITS, TLB_MISSES);
+
+    while (fread(NEW_ADDRESS, 2, 1, fp) == 1) {
+        int tlbHit = 0;
+        unsigned int PPN;
+        unsigned int VPN;
+        unsigned int offset;
+        unsigned int TRANSLATED_PHYSICAL_ADDRESS;
+
+        VPN = *NEW_ADDRESS / *PAGE_SIZE_B; // Calculate the Virtual Page Number
+        // printf("\nVPN:%hu\n",VPN);
+        for (int i = 0; i < TLB_ENTRIES; i++) {
+            // printf("TLB[%d]: %hu\n",i,TLB[i].vpn);
+            if (TLB[i].vpn == VPN) {
+                PPN = TLB[i].ppn;
+                offset = *NEW_ADDRESS % *PAGE_SIZE_B;
+                TRANSLATED_PHYSICAL_ADDRESS = (PPN * *PAGE_SIZE_B) + offset;
+                tlbHit = 1;
+                TLB[i].referenced = 1;
+                TLB_HITS++;
+                // printf("Inside");
+                break;
+            }
+        }
+        if(!tlbHit){
+            if(REPL == 0){
+                TLB_MISSES++;
+                PPN = PAGE_TABLE[VPN].ppn;
+                offset = *NEW_ADDRESS % *PAGE_SIZE_B;
+                TRANSLATED_PHYSICAL_ADDRESS = (PPN * *PAGE_SIZE_B) + offset;
+                TLB[tlbIndex].vpn = VPN;
+                TLB[tlbIndex].ppn = PPN;
+                tlbIndex++;
+                if(tlbIndex == TLB_ENTRIES){
+                    tlbIndex = 0;
+                }
+            }
+            else if(REPL == 1){
+                TLB_MISSES++;
+                PPN = PAGE_TABLE[VPN].ppn;
+                offset = *NEW_ADDRESS % *PAGE_SIZE_B;
+                TRANSLATED_PHYSICAL_ADDRESS = (PPN * *PAGE_SIZE_B) + offset;
+                int replaceIndex = rand() % *TLB_SIZE;
+                TLB[replaceIndex].vpn = VPN;
+                TLB[replaceIndex].ppn = PPN;
+            }
+            else if(REPL == 2){
+                PPN = PAGE_TABLE[VPN].ppn;
+                offset = *NEW_ADDRESS % *PAGE_SIZE_B;
+                TRANSLATED_PHYSICAL_ADDRESS = (PPN * *PAGE_SIZE_B) + offset;
+                int replaced_entry_index = -1;
+                while (1) {
+                    clock_hand = (clock_hand + 1) % *TLB_SIZE;
+                    if (TLB[clock_hand].referenced == 0) {
+                        replaced_entry_index = clock_hand;
+                        break;
+                    } else {
+                        TLB[clock_hand].referenced = 0; // Reset the reference bit
+                    }
+                }
+
+                // Update the TLB with the new entry
+                TLB[replaced_entry_index].vpn = VPN;
+                TLB[replaced_entry_index].ppn = PPN;
+                TLB[replaced_entry_index].referenced = 1;
+                clock_hand = replaced_entry_index;
+
+                TLB_MISSES++;
+            }
+            else{
+                printf("The replacement Police number is wrong");
+                exit(-1);
+            }
+        }
+        // Print the Virtual Address and Translated Physical Address
+        printf("VA:%x -- PA:%x\n", *NEW_ADDRESS, TRANSLATED_PHYSICAL_ADDRESS);
+    }
+    // Below is how the print statement should be formatted
+    printf("Page Faults: %d\nTLB hits: %d\nTlb misses: %d\n", PAGE_FAULTS, TLB_HITS, TLB_MISSES);
     free(PAGE_SIZE_B);
     free(NEW_ADDRESS);
-    free(PPN);
     free(TLB_SIZE);
+    free(PAGE_TABLE);
     fclose(fp);
 }
